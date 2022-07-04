a4b66425b10e3e4a2c5b50d833a594deb8d7b627
Victor Maciel <victormaciel0216@gmail.com>
2022-07-04 12:01:43 -0300

    aplica teste de persistencia em entidades com pk nao incremental

--- src/main/java/org/letscode/shoppingcart/Aplicacao.java
+++ src/main/java/org/letscode/shoppingcart/Aplicacao.java
@@ -2,6 +2,7 @@ package org.letscode.shoppingcart;
 
 import org.letscode.shoppingcart.core.data.EntityManager;
 import org.letscode.shoppingcart.core.persistence.InMemoryPersistor;
+import org.letscode.shoppingcart.model.Cliente;
 import org.letscode.shoppingcart.model.Produto;
 
 public class Aplicacao {
@@ -9,10 +10,14 @@ public class Aplicacao {
         EntityManager em = new EntityManager(new InMemoryPersistor());
         Produto produto = new Produto("Produto 1");
         Produto produto2 = new Produto("Produto 2");
+        Cliente cliente = new Cliente("999.999.999-99");
 
         em.save(produto);
         em.save(produto2);
+        em.save(cliente);
 
         System.out.println(em.get(1, Produto.class));
+        System.out.println(em.get(2, Produto.class));
+        System.out.println(em.get("999.999.999-99", Cliente.class));
     }
 }
--- src/main/java/org/letscode/shoppingcart/model/Cliente.java
+++ src/main/java/org/letscode/shoppingcart/model/Cliente.java
@@ -1,7 +1,26 @@
 package org.letscode.shoppingcart.model;
 
+import org.letscode.shoppingcart.core.annotations.PK;
 import org.letscode.shoppingcart.core.entity.Entity;
 
 public class Cliente extends Entity<String> {
-    
+    @PK
+    private String cpf;
+
+    public Cliente(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [cpf=" + cpf + "]";
+    }
 }
